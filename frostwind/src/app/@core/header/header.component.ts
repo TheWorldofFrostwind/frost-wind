799b92cd4f1ecd045252d8659786ba91221421de
Rizwan <expectp@gmail.com>
2023-01-23 05:24:00 +0500

    T-9: Set up custom and global styles + worked on header component

--- frostwind/src/app/@core/header/header.component.ts
+++ frostwind/src/app/@core/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('class.mobile') isMobile: boolean = false;
+  @HostBinding('class.desktop') isDesktop: boolean = false;
 
-  constructor() { }
+  constructor(private observer: BreakpointObserver) {}
 
   ngOnInit(): void {
+    this.observer
+      .observe('(max-width: 974px)')
+      .pipe(map((value) => value.matches))
+      .subscribe((isMobile) => {
+        this.isMobile = isMobile;
+        this.isDesktop = !isMobile;
+      });
   }
-
 }
